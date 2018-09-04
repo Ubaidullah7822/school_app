70ffd3184e7d4126453b83cc8f61341f8f0b17a0
Hassan Akram <hassan.akram3282@gmail.com>
2018-09-04 20:57:30 +0500

    error registering view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,4 +13,36 @@ class StudentsController < ApplicationController
 
   def create
   end
+
+  def register
+    @student = Student.find(params[:id])
+  end
+
+  def assign
+    puts "---------------------",params.inspect
+    course_name = params[:course]
+    student_id = params[:id]
+    course = Course.select(name: "#{course_name}")
+    puts "--------#{course.id}"
+    puts "--------#{student_id}"
+    stdcourse = StudentCourse.create(course_id: course.id, student_id: student_id)
+    binding.pry
+    # puts "--------working"
+    # stdcourse.student_id = student_id
+    # puts "--------working-1"
+    # stdcourse.course_id = course_id
+    # puts "--------working-2"
+    if(stdcourse.save)
+      puts "-----saved"
+    else
+      puts "--------again"
+      render "register"
+    end
+  end
+
+  private
+   def register_params
+    params.require(:student_course).permit(:student_id,:course_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students
+  resources :students do
+    member do
+      get :register
+      post :assign
+    end
+  end
   resources :teachers
   resources :courses
   root to: "students#index"
